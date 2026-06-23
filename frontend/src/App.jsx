352d7126984857d3c7beaba1c2446791441e7bec
Aditya Dwivedi <aditya22dwivedi22@gmail.com>
2026-06-23 19:02:53 +0530

    Learning React for this project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 
 function App() {
+
+  let [cnt, setCounter] = useState(15)
+
+  const addValue = () => {
+    setCounter(cnt+1)
+    console.log(cnt);
+    
+  }
+
+  const removeValue = () => {
+    setCounter(cnt-1)
+    console.log(cnt);
+    
+  }
+
   return (
     <div>
-      <h1>MessTrack</h1>
-      <p>Frontend is working 🚀</p>
+      <h2>Click any button</h2>
+      <button onClick={addValue}>Increment</button>
+      <button onClick={removeValue}>Decrement</button>
+      <br />
+      <h3>{cnt}</h3>
+      <h3>{cnt}</h3>
+      <h3>{cnt}</h3>
     </div>
   );
 }
